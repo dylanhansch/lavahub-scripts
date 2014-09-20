beb6f47ce7dcd88caaa1ddd7e1ea11605a92c5c6
Dylan Hansch <dylan@dylanhansch.net>
2014-09-20 10:46:28 -0500

    No auto restart
    We don't do automatic restarts anymore.

--- Minecraft/README.md
+++
@@ -1,4 +0,0 @@
-Minecraft
-===============
-
-Cron: * 0,4,8,12,16,20 * * * /root/lavahub/restart.sh
--- Minecraft/wrapper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-screen -S lavahub java -Xmx4096M -Xms4096M -Xincgc -XX:MaxPermSize=128M -jar spigot.jar nogui
